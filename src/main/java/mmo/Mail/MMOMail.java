ab85fc897db82247fde7378b02adcba2bc5b413f
Wulfspider <hello@wulfspider.com>
2012-05-16 11:32:58 -0400

    Updated pom.xml, readme, plugin.yml, and added header.txt. Added automatic license header checking and formatting. Formatted headers to be project specific. A little bit more formatting on misc files.

--- src/main/java/mmo/Mail/MMOMail.java
+++ src/main/java/mmo/Mail/MMOMail.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoMail <http://github.com/mmoMinecraftDev/mmoMail>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoMail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
